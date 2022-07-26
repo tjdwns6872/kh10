ab53ed76c2d422375694b79ff9c99998361cfbb1
tjdwns6872 <tjdwns6872@naver.com>
2022-07-26 11:35:27 +0900

    계산식 출력

---
+++ day02/src/data/Test02.java
@@ -0,0 +1,14 @@
+package data;
+//(Q)자장면 5천원, 짬뽕 6천원일 때 자장면 2그릇 + 짬뽕 3그릇 가격?
+import java.lang.*;
+
+public class Test02 {
+	public static void main(String[] args) {
+		int x = 5000; //자장면 변수 선언
+		int y = 6000; //짬뽕 변수 선언
+	
+		//System.out.println(28000);
+		//System.out.println(5000*2 + 6000*3); //계산식 출력
+		System.out.println((x*2)+(y*3)); //변수로 계산식 출력
+	}
+}
