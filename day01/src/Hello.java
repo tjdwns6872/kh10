f96ecb8e537b9d5616d45e5d2f54f591d42975a0
wooseog <tjdwns6872@naver.com>
2022-07-26 10:25:22 +0900

    주석 추가

--- day01/src/Hello.java
+++ day01/src/Hello.java
@@ -1,6 +1,8 @@
+//import 준비 명령
 import java.lang.*;
 public class Hello {
 	public static void main(String[] args) {
+		//출력 코드 - 콘솔창에 글자 출력
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 	}
