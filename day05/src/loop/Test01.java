372d533a7f08d14fab89a945a43308262a95aa0b
tjdwns6872 <tjdwns6872@naver.com>
2022-07-29 10:40:40 +0900

    for문 예제

---
+++ day05/src/loop/Test01.java
@@ -0,0 +1,18 @@
+package loop;
+import java.lang.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//loop
+		//(ex) 화면에 Hello를 10번 출력하세요
+		
+		//x10{
+		//for(1;2;3){코드}
+		//1 - 반복에 필요한 변수(반복수)를 만드는 영역(선언부)
+		//2 - 반복이 실행되어야 하는 조건을 지정하는 영역(조건부)
+		//3 - 반복수를 변화시키는 영역(증감부)
+		for(int i = 0; i <= 9; i++) {
+			System.out.println("Hello");
+		}
+	}
+
+}
