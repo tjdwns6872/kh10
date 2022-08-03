31eeabe0746c28e4e986ea8f5bb398a309c34dc0
tjdwns6872 <tjdwns6872@naver.com>
2022-08-03 09:56:44 +0900

    버블정렬

--- day07/src/array/Test13.java
+++ day07/src/array/Test13.java
@@ -7,7 +7,7 @@ public class Test13 {
 		int[] num = new int[] {30, 10, 20, 50, 40}; //배열 선언
 		for(int i = 0; i < num.length; i++) { //0~num배열 크기만큼 반복
 			for(int j = 0; j < num.length-1; j++) { //0~num배열의 크기만큼 반복
-				if(num[j] > num[j+1]) { //만약에 num[j]보다 num[j+1]이 크다면
+				if(num[j] < num[j+1]) { //만약에 num[j]보다 num[j+1]이 크다면
 					int tmp = num[j]; //새로운 변수에 num[j]값을 저장
 					num[j] = num[j+1]; //num[j]값을 num[j+1]값으로 변경 
 					num[j+1] = tmp; //tmp값 보관
