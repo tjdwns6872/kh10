365c874f05b105bdee76f4ee82ca7f2e42a91f96
tjdwns6872 <tjdwns6872@naver.com>
2022-08-05 11:23:57 +0900

    setter method 문제 예제

---
+++ day10/src/oop/setter1/Car.java
@@ -0,0 +1,43 @@
+package oop.setter1;
+
+public class Car {
+	//field
+	String name;
+	int speed;
+	int passenger;
+	int price;
+
+	//setter method
+	// - 이름을 지을 때 set+변수명으로 작성
+	void setName(String name) {
+		this.name = name;
+	}
+	void setSpeed(int speed) {
+		if(speed > 0) {
+			this.speed = speed;			
+		}
+	}		
+	void setPassenger(int passenger) {
+//		[1] passenger가 0보다 클 경우에만 설정하세요
+//			if(passenger > 0) {
+//				this.passenger = passenger;
+//		}
+//		[2] passenger가 0보다 작거나 같을 경우에만 설정하세요
+//		if(passenger <= 0) {
+//			this.passenger = passenger;
+//		}
+		this.passenger = passenger;
+	}
+	void setPrice(int price) {
+		if(price > 1000000) {
+			this.price = price;
+		}
+	}
+	void print(){
+		System.out.println(" < 차량 정보 > ");
+		System.out.println("차량 이름 : "+this.name);
+		System.out.println("차량의 속도 : "+this.speed);
+		System.out.println("탑승가능 인원 : "+this.passenger);
+		System.out.println("차량의 가격 : "+this.price);
+	}
+}
---
+++ day10/src/oop/setter1/Test01.java
@@ -0,0 +1,21 @@
+package oop.setter1;
+public class Test01 {
+
+	public static void main(String[] args) {
+		Car a = new Car();
+		
+		//(문제점) 변수는 이상 데이터 방지가 불가능
+		//a.name = "아반떼";
+		//a.speed = -200;
+		//a.passenger = -10;
+		//a.price = 15000000;
+		
+		//(해결책) 메소드는 필터링이 가능
+		a.setName("아반떼");
+		a.setSpeed(-10);
+		a.setPassenger(2);
+		a.setPrice(10000000);
+		
+		a.print();
+	}
+}
