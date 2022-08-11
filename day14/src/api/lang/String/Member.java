b1e164795dd592d3987b4e494f0cde6d530ed206
tjdwns6872 <tjdwns6872@naver.com>
2022-08-11 12:07:53 +0900

    로그인 처리프로그램(객체생성 및 equals())

---
+++ day14/src/api/lang/String/Member.java
@@ -0,0 +1,41 @@
+package api.lang.String;
+
+import java.util.Objects;
+
+public class Member {
+	private String id;
+	private String password;
+	
+	public Member(String id, String password) {
+		this.setId(id);
+		this.setPassword(password);
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, password);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		return Objects.equals(id, other.id) && Objects.equals(password, other.password);
+	}
+	
+}
