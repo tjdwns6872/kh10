e2daa24843acf5e8469f8f92cfc9d429d68af27e
tjdwns6872 <tjdwns6872@naver.com>
2022-08-12 14:53:57 +0900

    문자열 합성 문제

---
+++ day15/src/api/lang/etc/Test04.java
@@ -0,0 +1,21 @@
+package api.lang.etc;
+
+public class Test04 {
+	public static void main(String[] args) {
+		//String 합성(StringBuffer, StringBuilder)
+		// -문자열의 덧셈이 성능이 좋지 않은 이유는 무자열이 불변(immutable)이기 때문
+		
+		//별 생성 코드
+		
+		//10개
+		String star = "";
+		long a = System.currentTimeMillis();
+		for(int i = 0; i < 1000000; i++) {
+			star += "*";
+		}
+		long b = System.currentTimeMillis();
+		System.out.println(b - a);
+		//System.out.println(star);
+		
+	}
+}
