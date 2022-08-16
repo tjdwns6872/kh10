d9f0e3811c54b3aa0f5b488854b86aff8f0975a9
tjdwns6872 <tjdwns6872@naver.com>
2022-08-16 11:52:51 +0900

    시간 생성

---
+++ day16/src/api/util/date/Test01.java
@@ -0,0 +1,14 @@
+package api.util.date;
+import java.util.*;
+public class Test01 {
+	public static void main(String[] args) {
+		//시간 다루기
+		Date a = new Date();
+		Date b = new Date(2022, 8, 16);
+		Date c = new Date(22, 7, 16);
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+	}
+}
