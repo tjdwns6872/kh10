b3bd96de1bd7e99546f6f47e594ac76998a56ab9
tjdwns6872 <tjdwns6872@naver.com>
2022-08-17 16:02:12 +0900

    Set 로또 추첨 문제

---
+++ day17/src/api/util/collection2/Test02.java
@@ -0,0 +1,21 @@
+package api.util.collection2;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test02 {
+	public static void main(String[] args) {
+		/*로또 번호 추첨 문제를 Set으로 풀어보세요*/
+		
+		Set<Integer> list = new TreeSet<>();
+		while(true) {
+			Random rand = new Random();
+			list.add(rand.nextInt(45)+1);
+			if(list.size() == 6) {
+				break;
+			}
+		}
+		System.out.println(list);
+	}
+}
