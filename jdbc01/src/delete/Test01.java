93ad4ac5201eb9e8838ab1c962c0912b442dccc6
tjdwns6872 <tjdwns6872@naver.com>
2022-08-25 15:46:59 +0900

    삭제 예제

---
+++ jdbc01/src/delete/Test01.java
@@ -0,0 +1,26 @@
+package delete;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import util.JdbcUtil;
+
+public class Test01 {
+	public static void main(String[] args) {
+		//삭제(delete)
+		//delete fifa where country = ?;
+		
+		JdbcTemplate template = JdbcUtil.getTemplate();
+		
+		String country = "스페인";
+		
+		String sql = "delete fifa where country = ?";
+		Object[] param = {country};
+		
+		int result = template.update(sql, country);
+		if(result > 0) {
+			System.out.println("완료");
+		}else {
+			System.out.println("존재하지 않는 나라 이름입니다.");
+		}
+	}
+}
