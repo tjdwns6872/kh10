c286e576ad6d39ec9e3474198218b154812e66e3
tjdwns6872 <tjdwns6872@naver.com>
2022-08-26 16:04:12 +0900

    select sum

---
+++ jdbc03/src/app/Test08.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import util.JdbcUtil;
+
+public class Test08 {
+	public static void main(String[] args) {
+		// .class는 모든 클래스에 자동ㅇ로 생기는 클래스 정보 객체
+		JdbcTemplate template = JdbcUtil.getTemplate();
+		String sql = "select sum(music_play) from music";
+		int playCount = template.queryForObject(sql, int.class);
+		System.out.println(playCount);
+	}
+}
