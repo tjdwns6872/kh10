0777786ff6dcf936828a60fa113a8ace5fc3f706
tjdwns6872 <tjdwns6872@naver.com>
2022-08-31 14:11:52 +0900

    controller dto dao

---
+++ spring09dao/src/main/java/com/kh/spring09/repository/GuestBookDao.java
@@ -0,0 +1,15 @@
+package com.kh.spring09.repository;
+
+import java.util.List;
+
+import com.kh.spring09.entity.GuestBookDto;
+
+//DAO(Data Access Object)의 목차(추상체)
+public interface GuestBookDao {
+	void insert(GuestBookDto dto); //등록 기능
+	boolean update(GuestBookDto dto); //수정 기능
+	boolean delete(int no); //삭제 기능
+	List<GuestBookDto> selectList();//목록 기능
+	List<GuestBookDto> selectList(String type, String keyword);//검색 기능
+	GuestBookDto selectOne(int no); 
+}
