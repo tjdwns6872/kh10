8a9f6e8b2bd04d5e574d2f319f3854d7a97e05db
tjdwns6872 <tjdwns6872@naver.com>
2022-09-01 15:47:53 +0900

    포켓몬스터 등록 페이지

--- spring11view/src/main/java/com/kh/spring11/controller/HomeController.java
+++ spring11view/src/main/java/com/kh/spring11/controller/HomeController.java
@@ -9,4 +9,14 @@ public class HomeController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/test05")
+	public String test05() {
+		return "/WEB-INF/views/tag/test05.jsp";
+	}
+	
+	@RequestMapping("/test06")
+	public String test06() {
+		return "/WEB-INF/views/tag/test06.jsp";
+	}
 }
