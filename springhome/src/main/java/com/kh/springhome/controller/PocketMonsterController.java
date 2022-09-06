7335301afe84c2cd2156488da3f4306ae5768036
tjdwns6872 <tjdwns6872@naver.com>
2022-09-06 13:54:14 +0900

    테이블 출력

--- springhome/src/main/java/com/kh/springhome/controller/PocketMonsterController.java
+++ springhome/src/main/java/com/kh/springhome/controller/PocketMonsterController.java
@@ -1,7 +1,10 @@
 package com.kh.springhome.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +42,13 @@ public class PocketMonsterController {
 	@RequestMapping("/insert_success")
 	public String insertSuccess() {
 		return "pocketmon/insertResult";
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		List<PocketMonsterDto> list = pocketMonsterDao.selectList();
+		model.addAttribute("list", list);
+		return "pocketmon/list";
 	}
 	
 }
