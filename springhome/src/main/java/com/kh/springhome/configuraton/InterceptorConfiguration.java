cdde4c28bcc36da664517c1c28be337ba2c5a1b4
tjdwns6872 <tjdwns6872@naver.com>
2022-09-14 00:29:52 +0900

    인터셉트 이해

--- springhome/src/main/java/com/kh/springhome/configuraton/InterceptorConfiguration.java
+++ springhome/src/main/java/com/kh/springhome/configuraton/InterceptorConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kh.springhome.interceptor.AdminInterceptor;
 import com.kh.springhome.interceptor.MemberInterceptor;
+import com.kh.springhome.interceptor.TestInterceptor;
 
 // 스프링 설정파일
 // - application.properties에서 설정하기 어려운 복잡한 설정을 구현
@@ -19,21 +20,26 @@ public class InterceptorConfiguration implements WebMvcConfigurer{
 	private MemberInterceptor memberInterceptor;
 	@Autowired
 	private AdminInterceptor adminInterceptor;
+	@Autowired
+	private TestInterceptor testInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		//registry에 추가하여 인터셉터가 작동하도록 설정
+		registry.addInterceptor(testInterceptor).
+			addPathPatterns("/**");
+		
 		registry.addInterceptor(memberInterceptor).
-		addPathPatterns(//인터셉터가 감시할 주소
+			addPathPatterns(//인터셉터가 감시할 주소
 				"/pocketmon/**",
 				"/music/detail",
 				"/member/**").
-		excludePathPatterns(//제외할 주소
+			excludePathPatterns(//제외할 주소
 				"/member/insert",
 				"/member/login");
 		
 		registry.addInterceptor(adminInterceptor).
-		addPathPatterns(
+			addPathPatterns(
 				"/guestbook/edit*", //방명록 수정페이지
 				"/guestbook/delete", //방명록 삭제페이지
 				"/music/**", //음원 전체
@@ -41,7 +47,7 @@ public class InterceptorConfiguration implements WebMvcConfigurer{
 				"/member/detail", //회원상세
 				"/member/edit*", //회원수정
 				"/member/delete"). //회원삭제
-		excludePathPatterns(
+			excludePathPatterns(
 				"/music/list", //음원 목록
 				"/music/detail"); //음원 상세
 	}
