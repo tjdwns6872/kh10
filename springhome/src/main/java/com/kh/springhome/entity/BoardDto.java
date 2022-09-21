525d336fb793f0fedca31c60193b10a8dd74c6ee
tjdwns6872 <tjdwns6872@naver.com>
2022-09-21 10:59:37 +0900

    DTO 변경

--- springhome/src/main/java/com/kh/springhome/entity/BoardDto.java
+++ springhome/src/main/java/com/kh/springhome/entity/BoardDto.java
@@ -13,4 +13,7 @@ public class BoardDto {
 	private String boardHead, boardWriter, boardTitle, boardContent;
 	private Date boardWritetime, boardUpdatetime;
 	private int boardRead, boardLike;
+	// 계층형 게시판 데이터
+	// (참고) boardParent는 null일 수 있지만 JDBC에서 0으로 변환(Integer로 생성해야 됨)
+	private int boardGroup, boardParent, boardDepth;
 }
