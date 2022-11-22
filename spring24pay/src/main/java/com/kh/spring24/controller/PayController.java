d58e1f586836a4cc352e2717043cdda4feea448e
tjdwns6872 <tjdwns6872@naver.com>
2022-11-22 11:38:03 +0900

    결제

--- spring24pay/src/main/java/com/kh/spring24/controller/PayController.java
+++ spring24pay/src/main/java/com/kh/spring24/controller/PayController.java
@@ -1,6 +1,8 @@
 package com.kh.spring24.controller;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
@@ -16,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.spring24.entity.MemberDto;
+import com.kh.spring24.entity.PaymentDetailDto;
+import com.kh.spring24.entity.PaymentDto;
 import com.kh.spring24.entity.ProductDto;
+import com.kh.spring24.repository.PaymentDao;
 import com.kh.spring24.repository.ProductDao;
 import com.kh.spring24.service.KakaoPayService;
 import com.kh.spring24.vo.KakaoPayApproveRequestVO;
@@ -34,7 +39,7 @@ public class PayController {
 	private SqlSession sqlSession;
 	
 	@Autowired
-	private ProductDao productDao;
+	private PaymentDao paymentDao;
 
 	@RequestMapping("/")
 	public String home() {
@@ -63,7 +68,7 @@ public class PayController {
 		return "redirect:/";
 	}
 	
-//	결제 페이지 1번 - 입력창에 입력하면 결재되도록 구현
+	//결제 페이지 1번 - 입력창에 입력하면 결제되도록 구현
 	@GetMapping("/pay1")
 	public String pay1() {
 		return "pay1";
@@ -73,7 +78,8 @@ public class PayController {
 	private KakaoPayService kakaoPayService;
 	
 	@PostMapping("/pay1")
-	public String pay1(@ModelAttribute KakaoPayReadyRequestVO vo,
+	public String pay1(
+			@ModelAttribute KakaoPayReadyRequestVO vo,
 			HttpSession session) throws URISyntaxException {
 		String memberId = (String)session.getAttribute("loginId");
 		vo.setPartner_user_id(memberId);
@@ -81,7 +87,7 @@ public class PayController {
 		
 		KakaoPayReadyResponseVO response = kakaoPayService.ready(vo);
 		
-//		4번 시점에서 9번 시점에 필요한 데이터를 세션에 저장
+		//4번 시점에서 9번 시점에 필요한 데이터를 세션에 저장
 		session.setAttribute("tid", response.getTid());
 		session.setAttribute("partner_order_id", vo.getPartner_order_id());
 		session.setAttribute("partner_user_id", vo.getPartner_user_id());
@@ -89,17 +95,21 @@ public class PayController {
 		return "redirect:"+response.getNext_redirect_pc_url();
 	}
 	
-//	결제 성공 시 자동 호출될 주소
+	//결제 성공 시 자동 호출될 주소
 	@GetMapping("/pay/result/success")
-	public String paySuccess(@RequestParam String pg_token, HttpSession session) throws URISyntaxException {
-		
-//		9번 시점에서 4번 시점에서 세션에 저장한 데이터를 추출 및 삭제
+	public String paySuccess(
+			HttpSession session, @RequestParam String pg_token) throws URISyntaxException {
+		//9번 시점에서 4번 시점에서 세션에 저장한 데이터를 추출 및 삭제
 		String tid = (String)session.getAttribute("tid");
 		String partner_order_id = (String)session.getAttribute("partner_order_id");
 		String partner_user_id = (String)session.getAttribute("partner_user_id");
+		List<ProductDto> list = (List<ProductDto>)session.getAttribute("list");
+		List<PurchaseItemVO> data = (List<PurchaseItemVO>)session.getAttribute("data");
 		session.removeAttribute("tid");
 		session.removeAttribute("partner_order_id");
 		session.removeAttribute("partner_user_id");
+		session.removeAttribute("list");
+		session.removeAttribute("data");
 		
 		KakaoPayApproveRequestVO vo = KakaoPayApproveRequestVO
 				.builder()
@@ -109,13 +119,46 @@ public class PayController {
 				.pg_token(pg_token)
 				.build();
 		KakaoPayApproveResponseVO response = kakaoPayService.approve(vo);
+		
+//		이 시점에서 데이터베이스에 모든 정보를 INSERT 해야 한다.
+//		- 결제가 완료되었으니까
+//		(1) 대표정보 insert 후 (2) 상세정보 insert
+		int paymentNo = paymentDao.paymentSequence();
+		paymentDao.paymentInsert(PaymentDto.builder()
+					.paymentNo(paymentNo)
+					.memberId((String)session.getAttribute("loginId"))
+					.itemName(response.getItem_name())
+					.totalAmount(response.getAmount().getTotal())
+					.approveAt(response.getApproved_at())
+					.tid(response.getTid())
+				.build());
+		
+		for(int i=0; i<list.size(); i++) {
+			ProductDto productDto = list.get(i);
+			PurchaseItemVO itemVO = data.get(i);
+			int paymentDetailNo = paymentDao.paymentDetailSequence();
+			paymentDao.paymentDetailInsert(
+					PaymentDetailDto.builder()
+						.paymentDetailNo(paymentDetailNo)
+						.paymentNo(paymentNo)
+						.productNo(productDto.getNo())
+						.productName(productDto.getName())
+						.qty(itemVO.getQty())
+						.productPrice(productDto.getPrice() * itemVO.getQty())
+					.build()
+					);
+		}
+		
 		return "redirect:/pay/result/success_view";
 	}
-	
+
 	@GetMapping("/pay/result/success_view")
 	public String successView() {
 		return "success_view";
 	}
+	
+	@Autowired
+	private ProductDao productDao;
 	
 	@GetMapping("/pay2")
 	public String pay2(Model model) {
@@ -124,57 +167,55 @@ public class PayController {
 	}
 	
 	@PostMapping("/pay2")
-	public String pay2(HttpSession session,
+	public String pay2(HttpSession session, 
 			@ModelAttribute PurchaseVO purchaseVO) throws URISyntaxException {
-//		카카오페이에 보낼 정보를 계산
-//		- 상품이 1개면 - 상품 이름이 결제 상품 명이고 결제 금액은 상품금액 * 수량
-//			- ex : 500원짜리 초코파이 4개를 사면 결제명="초코파이" 금액=1500
-		
-//		- 상품이 여러개면 상품명은 첫 번째 상품명 외 ?개로 한다.
-//			- ex : 500원짜리 초코파이 2개와 300원 짜리 마이쮸3개
-//			-> 상품명 = 초코파이 외 1개 , 금액 = 1900
-		
+		//카카오페이에 보낼 정보를 계산
+		//- 상품이 1개면 - 상품 이름이 결제 상품 명이고 결제 금액은 상품금액 * 수량
+		//		- ex : 500원짜리 초코파이 3개를 사면 결제명="초코파이" 금액=1500 
+		//- 상품이 여러개면 상품명은 첫 번째 상품명 외 ?개로 한다
+		//		- ex : 500원짜리 초코파이2개와 300원짜리 마이쮸3개
+		//		-> 상품명=초코파이 외 1개, 금액=1900
 		if(purchaseVO.getData().isEmpty()) {
 			return "redirect:pay2";
 		}
 		
-//		첫 번째 상품만 따로 불러서 이름과 금액을 추축
-		PurchaseItemVO firstVO = purchaseVO.getData().get(0);
-		ProductDto firstProductDto = productDao.find(firstVO.getNo());
-		String item_name = firstProductDto.getName();
-		if(purchaseVO.getData().size()>=2) { // 상품이 2개 이상이면
-			int value = purchaseVO.getData().size()-1;
-			item_name += "외"+value+"개";
-		}
-		int total_amount = firstProductDto.getPrice() * firstVO.getQty();
-		
-//		두 번째 이후의 상품을 반복하여 합계를 합산
-		for(int i = 1; i < purchaseVO.getData().size(); i++) { //no, qty를 추출
-			PurchaseItemVO itemVO = purchaseVO.getData().get(i);
-			ProductDto productDto = productDao.find(itemVO.getNo()); // 상품정보 조회
-			int total = productDto.getPrice() * itemVO.getQty(); // 상품 구매 금액 계산
-			total_amount += total;
+//		(+추가) - 결제 정보를 등록하려면 반드시 상품 정보를 알아야 한다(List<ProductDto>)
+		List<ProductDto> list = new ArrayList<>();
+
+		int total_amount = 0;
+		for(PurchaseItemVO vo : purchaseVO.getData()) {
+			ProductDto productDto = productDao.find(vo.getNo());
+			list.add(productDto);
+			total_amount += productDto.getPrice() * vo.getQty();
 		}
 		
+//		- 이름 설정
+		String item_name = list.get(0).getName(); // 첫 번째 상품명 추출
+		if(list.size() >= 2) {
+			item_name += "외" +(list.size()-1)+"개";
+		}
+				
 		KakaoPayReadyRequestVO vo = KakaoPayReadyRequestVO
 				.builder()
 				.partner_order_id(UUID.randomUUID().toString())
 				.partner_user_id((String)session.getAttribute("loginId"))
 				.item_name(item_name)
-				.total_amount(total_amount).
-				build();
+				.total_amount(total_amount)
+				.build();
 		
 		KakaoPayReadyResponseVO response = kakaoPayService.ready(vo);
 		
-//		4번 시점에서 9번 시점에 필요한 데이터를 세션에 저장
+		//4번 시점에서 9번 시점에 필요한 데이터를 세션에 저장
 		session.setAttribute("tid", response.getTid());
 		session.setAttribute("partner_order_id", vo.getPartner_order_id());
 		session.setAttribute("partner_user_id", vo.getPartner_user_id());
+		session.setAttribute("list", list); // 상품 목록
+		session.setAttribute("data", purchaseVO.getData()); // 상품수량 목록
 		
 		return "redirect:"+response.getNext_redirect_pc_url();
 	}
 	
-//	주문 조회 페이지
+	//주문 조회 페이지
 	@GetMapping("/detail")
 	public String detail(@RequestParam String tid, Model model) throws URISyntaxException {
 		KakaoPayOrderRequestVO vo = KakaoPayOrderRequestVO
@@ -183,14 +224,3 @@ public class PayController {
 		return "detail";
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
